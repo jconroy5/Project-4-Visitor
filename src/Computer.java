bb5c4ae6489c448b72bb69df1a650e04c1dbbce9
jconroy5 <jconroy5@luc.edu>
2019-04-17 10:42:34 -0500

    propagated Computer as Peripheral implementation

--- src/Computer.java
+++ src/Computer.java
@@ -1,3 +1,15 @@
-public class Computer {
+public class Computer implements Peripheral{
+    Peripheral[] computerPeripherals;
 
+    public Computer(){
+        computerPeripherals = new Peripheral[] {new Keyboard(), new Mouse(), new Monitor()};
+    }
+
+    @Override
+    public void accept(ComputerVisitor computerVisitor){
+        for(int i = 0; i < computerPeripherals.length; i++){
+            computerPeripherals[i].accept(computerVisitor);
+        }
+        computerVisitor.visit(this);
+    }
 }
