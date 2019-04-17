1d0cc2c0c55e3824e80a25195f6d1411e3973a63
jconroy5 <jconroy5@luc.edu>
2019-04-17 10:49:09 -0500

    propagated PeripheralChecker

--- src/PeripheralChecker.java
+++ src/PeripheralChecker.java
@@ -1,3 +1,22 @@
 public class PeripheralChecker implements ComputerVisitor{
 
+    @Override
+    public void visit(Computer computer){
+        System.out.println("Checking system... \nComputer is operating normally!");
+    }
+
+    @Override
+    public void visit(Keyboard keyboard){
+        System.out.println("Checking keyboard... \nKeyboard is inputting normally!");
+    }
+
+    @Override
+    public void visit(Mouse mouse){
+        System.out.println("Checking mouse... \nMouse is inputting normally!");
+    }
+
+    @Override
+    public void visit(Monitor monitor){
+        System.out.println("Checking monitor... \nMonitor is displaying normally!");
+    }
 }
